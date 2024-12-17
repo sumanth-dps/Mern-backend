84dca40b4aaeb8cb95aa3fc3d186e15253ef8792
sumanth-dps <dondamuripavansatyasumanth@gmail.com>
2024-12-17 13:30:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,11 +4,14 @@ import cors from "cors";
 import "dotenv/config";
 import cookieParser from "cookie-parser";
 import authRouter from "./routes/authRoutes.js";
-
+const allowedOrigins = [
+  "https://great-stack-authentication",
+  "http://great-stack-authentication",
+];
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin:"*", credentials: true }));
+app.use(cors({ origin:allowedOrigins, credentials: true }));
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
